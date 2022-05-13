ee0d3a40c8331b50269ee03975057bd4bf9ba309
carrotdip <92421274+carrotdip@users.noreply.github.com>
2022-05-12 18:22:10 -0700

    additional queries + updated Tableau dashboard

--- queries.sql
+++ queries.sql
@@ -39,3 +39,45 @@ ORDER BY SUM(profit) DESC
 LIMIT 25;
 
 -- 
+
+-- View entirety of data
+SELECT * FROM superstore;
+
+-- Total sales, discounts, and profits by state
+SELECT state, SUM(sales) as total_sales, SUM(discount) as total_discount, SUM(profit) as total_profit
+FROM superstore
+GROUP BY state
+ORDER BY total_profit DESC;
+-- From the Tableau dashboard, it is clear that 
+-- Texas, Ohio, Pennsylvania, and Illinois are the lowest-profit-producing states (< -$10,000)
+-- California, New York, Washington, and Michigan are the highest-profit-producing states (> $20,000)
+
+-- View category and sub_category by total_profits
+SELECT category, sub_category, SUM(profit) as total_profit
+FROM superstore
+GROUP BY category, sub_category
+ORDER BY total_profit;
+-- Tables & Bookcases sub-categories in Furniture category have lost the company the most money
+-- Copiers, Phones, and Accessories sub-categories in Technology category have generated the most profit
+
+-- How has tables and bookcases fared by region?
+SELECT region, category, sub_category, SUM(profit) as total_profit
+FROM superstore
+WHERE category = 'Furniture'
+AND sub_category = 'Tables'
+OR sub_category = 'Bookcases'
+GROUP BY region, category, sub_category
+ORDER BY region, total_profit;
+-- Tables & Bookcases have not done well in all regions, best to remove this from store catalog
+
+-- How have copiers, phones, and accessories fared by region?
+SELECT region, category, sub_category, SUM(profit) as total_profit
+FROM superstore
+WHERE category = 'Technology'
+AND sub_category = 'Copiers'
+OR sub_category = 'Phones'
+OR sub_category = 'Accessories'
+GROUP BY region, category, sub_category
+ORDER BY region, total_profit;
+-- Increase marketing/advertisement for these items
+
